6c6444ee654dad33ec690d3a3dcf90ba7ad3b3fa
VAISHNAVI\vaishnavi <vgandicheruvu@credomobile.com>
2017-08-17 13:12:32 -0500

    first commit

---
+++ app/book.component.ts
@@ -0,0 +1,11 @@
+import {Component} from '@angular/core';
+
+@Component({
+	selector:'book-app',
+	template:'<div><h1> This is a book comoent</h1></div>'
+   })
+
+export class BookComponent{}
+
+
+
